91b3740bbc14aa3e23dd31d304f304542f7d9196
Damian Suess <dsuess@xenoinc.org>
2020-07-24 18:07:29 -0400

    Updated output folders for projects

--- Project-Full/XamarinTemplate/XamarinTemplate.Droid/Properties/AssemblyInfo.cs
+++ Project-Full/XamarinTemplate/XamarinTemplate.Droid/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TemplateApp.Android")]
+[assembly: AssemblyTitle("TemplateApp.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TemplateApp.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("TemplateApp.Droid")]
+[assembly: AssemblyCopyright("Copyright © CompanyName 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
